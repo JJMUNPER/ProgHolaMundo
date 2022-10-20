b4dafe0890c7b0d8f3a126af41c0db17da2697bb
JJMUNPER <jmunper007@g.educaand.es>
2022-10-20 12:14:09 +0200

    Codigo hola mundo en consola y Netbeans

---
+++ HolaMundo2/src/main/java/com/jmunper/holamundo2/HolaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * Este codigo ha sido creado
+ * en el entorno de desarrollo
+ * de netbeans.
+ */
+package com.jmunper.holamundo2;
+
+/**
+ *
+ * @author 1DAW_2223_02
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("adios");
+    }
+}
